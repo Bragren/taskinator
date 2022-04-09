876474aa8fdd64747b0832efb5ea88145ee09156
Bragren <bragren17@gmail.com>
2022-04-09 15:22:50 -0600

    added alert to stop empty tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,20 @@ var taskFormHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-  
+
     // package up data as an object
     var taskDataObj = {
-      name: taskNameInput,
-      type: taskTypeInput
+        name: taskNameInput,
+        type: taskTypeInput
     };
-  
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
 
@@ -20,16 +27,16 @@ var taskFormHandler = function (event) {
 var createTaskEl = function (taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    
+
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     // give it a class name
     taskInfoEl.className = "task-info";
     // add HTML content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-    
+
     listItemEl.appendChild(taskInfoEl);
-    
+
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
